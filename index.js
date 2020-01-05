a8724044f1064e444a6cd65542137de40b36a986
Daniel Tribe <daniel.tribe@a113.net>
2020-01-05 19:21:46 +0000

    Updated and cleaned code, moved to contact sensor over CO2 sensor
    Signed-off-by: Daniel Tribe <daniel.tribe@a113.net>

--- index.js
+++ index.js
@@ -6,55 +6,42 @@ var Service, Characteristic;
 module.exports = function(homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
-  
+
   homebridge.registerAccessory("homebridge-plugin-uk-nationalgrid-carbonintensity", "National Grid Carbon Intensity", NGCarbonIntensityAccessory);
 }
 
 function NGCarbonIntensityAccessory(log, config) {
   this.log = log;
-  this.config = config;
-  this.name = "National Grid Carbon Intensity";
   
-  this.service = new Service.CarbonDioxideSensor(this.name);
+  this.highCarbonLevels = config.highCarbonLevels;
+
+  this.highService = new Service.ContactSensor("High National Grid Carbon Intensity");
   
-  this.service
-    .getCharacteristic(Characteristic.CarbonDioxideLevel)
+  this.highService
+    .getCharacteristic(Characteristic.ContactSensorState)
     .on('get', this.executeCo2.bind(this));
 }
 
 NGCarbonIntensityAccessory.prototype.executeCo2 = function(callback) {
-  this.log("Getting current state...");
-  
   request.get({
     url: "https://api.carbonintensity.org.uk/intensity",
     headers: {
       'Accept': 'application/json'
     }
-  }, function(err, response, body) {
-    
+  }, function(err, response, body) {    
     if (!err && response.statusCode == 200) {
       var json = JSON.parse(body);
       var data = json.data;
-      var actual = data[0].intensity.actual;
-      var forecast = data[0].intensity.forecast;
       var intensity = data[0].intensity.index;
-
-      if (isArray(this.config.highCarbonLevels)) {
-        var index = this.config.highCarbonLevels.indexOf(intensity) != -1
-        this.service.setCharacteristic(Characteristic.CarbonDioxideDetected, index);
+      
+      if (isArray(this.highCarbonLevels)) {
+        var index = this.highCarbonLevels.indexOf(intensity) != -1
+        callback(null, index);
       }
-
-      this.service.setCharacteristic(Characteristic.CarbonDioxideLevel, actual);
-      this.service.setCharacteristic(Characteristic.CarbonDioxidePeakLevel, forecast);
-      callback(null, actual);
-    }
-    else {
-      this.log("Error getting state (status code %s): %s", response.statusCode, err);
-      callback(err);
     }
   }.bind(this));
 }
 
 NGCarbonIntensityAccessory.prototype.getServices = function() {
-  return [this.service];
+  return [this.highService];
 }
