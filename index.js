a66daab4ec8d2732dc264ac94073af7c20b08ac0
Daniel Tribe <daniel.tribe@a113.net>
2020-01-06 20:45:15 +0000

    Version 1.3, moved to switch device and periodic updates
    Signed-off-by: Daniel Tribe <daniel.tribe@a113.net>

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var request = require("request");
-var isArray = require('yow/isArray');
+const request = require("request");
+const isArray = require('yow/isArray');
 
 var Service, Characteristic;
 
@@ -15,14 +15,16 @@ function NGCarbonIntensityAccessory(log, config) {
   
   this.highCarbonLevels = config.highCarbonLevels;
 
-  this.highService = new Service.ContactSensor("High National Grid Carbon Intensity");
+  this.highCo2IntensitySwitch = new Service.Switch("High National Grid Carbon Intensity");
   
-  this.highService
-    .getCharacteristic(Characteristic.ContactSensorState)
-    .on('get', this.executeCo2.bind(this));
+  this.highCo2IntensitySwitch
+    .getCharacteristic(Characteristic.On)
+    .on('get', this.getCurrentCo2IntensitySwitchState.bind(this));
+
+    this.startPing();
 }
 
-NGCarbonIntensityAccessory.prototype.executeCo2 = function(callback) {
+NGCarbonIntensityAccessory.prototype.getCurrentCo2IntensitySwitchState = function(callback) {
   request.get({
     url: "https://api.carbonintensity.org.uk/intensity",
     headers: {
@@ -36,12 +38,36 @@ NGCarbonIntensityAccessory.prototype.executeCo2 = function(callback) {
       
       if (isArray(this.highCarbonLevels)) {
         var index = this.highCarbonLevels.indexOf(intensity) != -1
+        this.log(index);
         callback(null, index);
       }
     }
   }.bind(this));
 }
 
+NGCarbonIntensityAccessory.prototype.startPing = function() {
+  setInterval(() => {
+    request.get({
+      url: "https://api.carbonintensity.org.uk/intensity",
+      headers: {
+        'Accept': 'application/json'
+      }
+    }, function(err, response, body) {    
+      if (!err && response.statusCode == 200) {
+        var json = JSON.parse(body);
+        var data = json.data;
+        var intensity = data[0].intensity.index;
+        
+        if (isArray(this.highCarbonLevels)) {
+          var index = this.highCarbonLevels.indexOf(intensity) != -1
+          this.log(index);
+          this.highCo2IntensitySwitch.setCharacteristic(Characteristic.On, index);
+        }
+      }
+    }.bind(this));
+  }, 60000);
+}
+
 NGCarbonIntensityAccessory.prototype.getServices = function() {
-  return [this.highService];
+  return [this.highCo2IntensitySwitch];
 }
